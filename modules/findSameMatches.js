e3287ad56ea0183b10bd3046271600ea7554518c
Patrik Pagáč <patrik.pagac@hyperia.sk>
2025-01-22 10:37:37 +0100

    find same matches module mvp

--- modules/findSameMatches.js
+++ modules/findSameMatches.js
@@ -2,6 +2,28 @@ import fs from 'fs'
 
 const BASE_DATA_FOLDER_PATH = process.cwd() + '/data'
 
+const filterMatchData = (match) => {
+	return match.startsAtTs > Date.now()
+}
+
+const formatMatchesData = (data) => {
+	return data.map((match) => {
+		delete match.bets
+
+		match.nameSplitted = match.name
+			.toLowerCase()
+			.normalize('NFD')
+			.replaceAll('. ', ' ')
+			.replaceAll('.', ' ')
+			.replaceAll(' / ', ' ')
+			.replaceAll('/', ' ')
+			.split(' ')
+			.filter((namePart) => namePart.length > 2)
+
+		return match
+	})
+}
+
 const loadData = () => {
 	const trackedSports = process.env.TRACKED_SPORTS?.split(',')
 	const enabledScrapers = process.env.ENABLED_SCRAPERS?.split(',')
@@ -14,16 +36,79 @@ const loadData = () => {
 		enabledScrapers?.map((scraperName) => {
 			const filePath = `${BASE_DATA_FOLDER_PATH}/${scraperName}/${sportName}.json`
 			const rawData = fs.readFileSync(filePath, 'utf-8')
-			const formattedData = JSON.parse(rawData).map((event) => {
-				delete event.bets
-				return event
+			const data = JSON.parse(rawData)
+			const formattedData = formatMatchesData(data)
+
+			const filteredData = formattedData.filter((match) => {
+				return filterMatchData(match)
 			})
 
-			result[sportName][scraperName] = formattedData
+			result[sportName][scraperName] = filteredData
 		})
 	})
 
 	return result
+}
+
+const findMatches = (data) => {
+	let foundMatches = []
+	let totalMatches = 0
+
+	const sports = Object.keys(data)
+
+	sports.forEach((sport) => {
+		const scraperNames = Object.keys(data[sport])
+
+		// find scraper file with the most entries
+		let largestArrayLength = 0
+		let largestArrayScraperName = scraperNames[0]
+
+		Object.keys(data[sport]).forEach((scraperName) => {
+			if (data[sport][scraperName].length > largestArrayLength) {
+				largestArrayLength = data[sport][scraperName].length
+				largestArrayScraperName = scraperName
+			}
+		})
+
+		totalMatches += largestArrayLength
+
+		const largestScraperMatches = data[sport][largestArrayScraperName]
+
+		largestScraperMatches.forEach((matchToFind) => {
+			Object.keys(data[sport]).forEach((scraperName) => {
+				if (scraperName === largestArrayScraperName) {
+					return
+				}
+
+				const currentScraperMatches = data[sport][scraperName]
+
+				currentScraperMatches.forEach((match) => {
+					const hasSimilarName =
+						matchToFind.nameSplitted.filter((namePart) =>
+							match.nameSplitted.includes(namePart)
+						).length > 1
+					const timeDiff = Math.abs(
+						matchToFind.startsAtTs - match.startsAtTs
+					)
+					// one hour
+					const maxTimeDiff = 1 * 60 * 60 * 1000
+
+					if (hasSimilarName && timeDiff <= maxTimeDiff) {
+						foundMatches.push({
+							sport,
+							[scraperName]: match.id,
+							[largestArrayScraperName]: matchToFind.id,
+						})
+					}
+				})
+			})
+		})
+	})
+
+	return {
+		foundMatches,
+		totalMatches,
+	}
 }
 
 export const findSameMatches = () => {
@@ -31,8 +116,13 @@ export const findSameMatches = () => {
 	console.log('looking for the same matches...')
 
 	const data = loadData()
+	const { foundMatches, totalMatches } = findMatches(data)
 
 	console.log(
-		`...same matches found in ${Math.round(performance.now() - now)}ms`
+		`...found ${
+			foundMatches.length
+		} from total ${totalMatches} matches in ${Math.round(
+			performance.now() - now
+		)}ms`
 	)
 }
